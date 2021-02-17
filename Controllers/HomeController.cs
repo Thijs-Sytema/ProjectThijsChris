aa6970ab5445433f4a5263607c1fc10bd9623ec0
T. Sytema <110443@st-maartenscollege.nl>
2021-02-17 11:59:52 +0100

    Over ons pagina aangemaakt, ook gelijk voor gezorgd dat er geen lelijke home in de URL staat

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -40,6 +40,12 @@ namespace ProjectThijsChris.Controllers
             return View();
         }
 
+        [Route("Overons")]
+        public IActionResult Overons()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
