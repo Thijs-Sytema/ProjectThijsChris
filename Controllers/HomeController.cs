5f0a5ab64053f3d7b18592a69345c608e6dac84d
110444@st-maartenscollege.nl <110444@st-maartenscollege.nl>
2021-05-17 12:39:32 +0200

    get names functie toegevoegd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Logging;
 using ProjectThijsChris.Models;
 using System.Diagnostics;
 using MySql.Data;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ProjectThijsChris.Controllers
 {
@@ -53,6 +55,42 @@ namespace ProjectThijsChris.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public List<string> GetNames()
+        {
+            // stel in waar de database gevonden kan worden
+            string connectionString = "Server=172.16.160.21;Port=3306;Database=fastfood;Uid=lgg;Pwd=0P%Y9fI2GdO#;";
+
+            // maak een lege lijst waar we de namen in gaan opslaan
+            List<string> names = new List<string>();
+
+            // verbinding maken met de database
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                // verbinding openen
+                conn.Open();
+
+                // SQL query die we willen uitvoeren
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
+
+                // resultaat van de query lezen
+                using (var reader = cmd.ExecuteReader())
+                {
+                    // elke keer een regel (of eigenlijk: database rij) lezen
+                    while (reader.Read())
+                    {
+                        // selecteer de kolommen die je wil lezen. In dit geval kiezen we de kolom "naam"
+                        string Name = reader["Naam"].ToString();
+
+                        // voeg de naam toe aan de lijst met namen
+                        names.Add(Name);
+                    }
+                }
+            }
+
+            // return de lijst met namen
+            return names;
         }
     }
 }
