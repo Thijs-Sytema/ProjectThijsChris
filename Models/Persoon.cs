66b3f3b7fdd419b1fa252090ff471fc03a44aaf8
T. Sytema <110443@st-maartenscollege.nl>
2021-06-07 12:20:19 +0200

    ErrorMessage toegevoegd

--- Models/Persoon.cs
+++ Models/Persoon.cs
@@ -4,11 +4,11 @@ namespace ProjectThijsChris.Models
 {
     public class Persoon
     {
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw voornaam in te vullen")]
         public string voornaam { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw achternaam in te vullen")]
         public string achternaam { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Gelieve uw emailadres in te vullen")]
         public string email { get; set; }
         public string telefoonnummer { get; set; }
         public string adres { get; set; }
