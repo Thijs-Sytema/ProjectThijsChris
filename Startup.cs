c8ad51fa4c19cf2fdb66aef899610c6b1422aa6b
Christiaan Oosterbeek <110444@st-maartenscollege.nl>
2021-06-11 21:53:36 +0200

    puntjes op i

--- Startup.cs
+++ Startup.cs
@@ -33,6 +33,15 @@ namespace ProjectThijsChris
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/error";
+                    await next();
+                }
+            });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
